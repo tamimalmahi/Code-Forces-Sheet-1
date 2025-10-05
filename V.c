fc46ad3644c10fe11995272beeefae368b1da60e
Tamim Al Mahi <rikabadnan@gmail.com>
2025-10-05 13:03:05 +0600

    Create V.c

---
+++ V.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int A,B;
+    char C;
+    scanf("%d %c %d",&A,&C,&B);
+    if(A > B && C == '>')
+        printf("Right");
+    else if(A < B && C == '<')
+        printf("Right");
+    else if(A == B && C == '=')
+        printf("Right");
+    else
+        printf("Wrong");
+    return 0;
+}
